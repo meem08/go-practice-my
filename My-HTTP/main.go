374f33a7af3cfb0e238072b46d6d9ce7cf0cd22f
bakinsuy <blessing.akinsuyi01@gmail.com>
2026-04-14 15:51:41 +0100

    MY first HTTP

--- My-HTTP/main.go
+++ My-HTTP/main.go
@@ -1,50 +1,57 @@
 package main
 
-import(
+import (
+	"html/template"
+	"fmt"
 	"net/http"
-	"encoding/json"
 )
-type User struct {
-		ID   int
-		Name string
-	}
 
-// func main() {
-// 	users := []User{
-// 		{ID: 1, Name: "Bola"},
-// 		{ID:  2, Name: "Fola"},
-// 	}
-	
-// 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/json")
 
-// 		err := json.NewEncoder(w).Encode(users)
-// 		if err != nil {
-// 			http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
-// 		}
-// 	})
-// 	err:= http.ListenAndServe(":3000", nil)
-// 	if err != nil {
-// 		fmt.Println("Server error: ", err)
-// 	}
-	
-	
-// }
-type Message struct {
-		Text string `json:"text"`
-	}
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	response := Message{Text: "Hello Blessing"}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	err := json.NewEncoder(w).Encode(response)
-	if err != nil {
-		http.Error(w, "Error encoding json", http.StatusInternalServerError)
-	}
+type Data struct{
+    Name string
 }
+ 
+var templ = template.Must(template.New("index.html").ParseFiles("./template/index.html"))
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/hello", HelloHandler)
+	http.HandleFunc("/generate", GenerateHandler)
+
+	fmt.Println("Sever listening on port 3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil{
+		
+	}
 }
+
+
+func HomeHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "text/html")
+	w.WriteHeader(200)
+	templ.Execute(w, nil)
+}
+
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(200)
+	w.Write([]byte("Hello blessing"))
+
+}
+func GenerateHandler(w http.ResponseWriter, r *http.Request) {
+     
+    if r.Method != http.MethodPost{
+         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+
+	r.ParseForm()
+      name := r.FormValue("myName")
+
+	 templ.Execute(w,  Data{
+		Name:name,
+	 })
+
+} 
